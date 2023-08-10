d230e54a0827a7c3c840d00c166b927b042a0603
larakoseff <larakoseff@gmail.com>
2023-08-10 14:23:31 +0200

    Total balance but incorrect, not sure why

--- challenge1/scripts.js
+++ challenge1/scripts.js
@@ -3,8 +3,9 @@ const secondaryPhone = '0219131568'
 
 // Only change below this line
 
-const primaryValid = typeof number === parseInt(primaryPhone) 
-const secondaryValid = typeof number === parseInt(secondaryPhone)
+
+const primaryValid = !isNaN(parseInt(primaryPhone))
+const secondaryValid = !isNaN(parseInt(secondaryPhone))
 
 console.log('Primary phone is valid numerical string:', primaryValid)
-console.log('Secondary phone is valid numerical string:', secondaryValid )
+console.log('Secondary phone is valid numerical string:', secondaryValid)
