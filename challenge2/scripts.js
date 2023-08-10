61de3f0129cbd3bc51d5a3ce36a829fdc53d5527
larakoseff <larakoseff@gmail.com>
2023-08-10 14:59:34 +0200

    Put * salary in taxAsDecimal, remove ex const

--- challenge2/scripts.js
+++ challenge2/scripts.js
@@ -8,26 +8,13 @@ const minuteOfDay = 0;
 
 // Only change below this line
 
-// To get the correct balance I found that I have to add in an extra variable
-// (taxPercCalc) to properly calculate the tax or else it just deducts 0.08,
-// instead of 8% 
 
 if (hourOfDay === 0 && minuteOfDay === 0) {
-    const taxAsDecimal = parseFloat(tax) / '100'
-    const taxPercCalc = salary * taxAsDecimal
-    const startingAfterTax = salary * (parseInt('1')) - taxPercCalc
+    const taxAsDecimal = parseFloat(tax) / 100 * salary
+    const startingAfterTax = salary * (parseInt('1')) - taxAsDecimal
     const balance =  startingAfterTax - transport - food - rent
 
     console.log('R', balance.toFixed(2))
 }
 
-// if I run this code balance is R337.97 
-
-// if (hourOfDay === 0 && minuteOfDay === 0) {
-//     const taxAsDecimal = parseFloat(tax) / 100
-//     const startingAfterTax = salary * (parseInt('1')) - taxAsDecimal
-//     const balance =  startingAfterTax - transport - food - rent
-
-//     console.log('R', balance.toFixed(2))
-// }
 
