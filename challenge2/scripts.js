0744f2d9e8241b0a308f880a99d84061bddfa698
larakoseff <larakoseff@gmail.com>
2023-08-10 14:44:17 +0200

    Edit typos

--- challenge2/scripts.js
+++ challenge2/scripts.js
@@ -8,8 +8,9 @@ const minuteOfDay = 0;
 
 // Only change below this line
 
-// To get the correct I found taht I have to add in an extra variable (taxPercCalc) to
-// properly calculate the tax or else it just deducts 0.08, instead of 8% 
+// To get the correct balance I found that I have to add in an extra variable
+// (taxPercCalc) to properly calculate the tax or else it just deducts 0.08,
+// instead of 8% 
 
 if (hourOfDay === 0 && minuteOfDay === 0) {
     const taxAsDecimal = parseFloat(tax) / '100'
@@ -20,7 +21,7 @@ if (hourOfDay === 0 && minuteOfDay === 0) {
     console.log('R', balance.toFixed(2))
 }
 
-// // if I run this code balance is R337.97 
+// if I run this code balance is R337.97 
 
 // if (hourOfDay === 0 && minuteOfDay === 0) {
 //     const taxAsDecimal = parseFloat(tax) / 100
