4689d16306b77ae1d0cab9b9e5f6c1b8d3b5724e
larakoseff <larakoseff@gmail.com>
2023-08-10 15:07:41 +0200

    Fix startingAfterTax const to get balance

--- challenge2/scripts.js
+++ challenge2/scripts.js
@@ -10,8 +10,8 @@ const minuteOfDay = 0;
 
 
 if (hourOfDay === 0 && minuteOfDay === 0) {
-    const taxAsDecimal = parseFloat(tax) / 100 * salary
-    const startingAfterTax = salary * (parseInt('1')) - taxAsDecimal
+    const taxAsDecimal = parseFloat(tax) / 100 
+    const startingAfterTax = salary * ((parseInt('1')) - taxAsDecimal)
     const balance =  startingAfterTax - transport - food - rent
 
     console.log('R', balance.toFixed(2))
